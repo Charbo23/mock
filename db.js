196a28383f3252705349d12541493c2a66a942ee
ChenYibo <charbo233@foxmail.com>
2020-01-10 17:24:07 +0800

    更新

--- db.js
+++ db.js
@@ -2,7 +2,9 @@ module.exports = () => {
   const photos = require("./mock/photos.json");
   const users = require("./mock/users.json");
   const tagList = require("./mock/tagList.json");
-  var mockSourceArr = [photos, users, tagList];
+  const tagInfo = require("./mock/tagInfo.json");
+
+  var mockSourceArr = [photos, users, tagList,tagInfo];
   let mockData = {};
   mockSourceArr.forEach(function(item) {
     mockData = Object.assign(mockData, item);
